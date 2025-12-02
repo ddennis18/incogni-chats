6555a48ca8e31ca8107e20ad08215f7d3396faff
ddennis18 <osemwonken18@gmail.com>
2025-12-02 15:51:25 +0100

    navbar added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { PlusIcon, Menu, X } from 'lucide-react'
+import { useState } from 'react'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className='w-full bg-base-300 px-8 py-2 flex flex-row justify-between items-center shadow-lg'>
+      <h1 className='logo'>INCOGNI</h1>
+      <nav className=''>
+        <ul className='hidden md:flex flex-row gap-4'>
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li className='w-max'>
+            New <PlusIcon />{' '}
+          </li>
+        </ul>
+        <span className='md:hidden' onClick={() => setIsOpen(prev => !prev)}>
+          {!isOpen && <Menu />}
+          {isOpen && <X/>}
+        </span>
+      </nav>
+      {isOpen && (
+        <ul className='bg-base-300 fixed flex flex-col gap-8 w-full p-4 top-15 left-0'>
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li className='w-max'>
+            New <PlusIcon />{' '}
+          </li>
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default Navbar
