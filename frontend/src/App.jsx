65edda8f72f0a71087150e65f538bbbb7facf461
ddennis18 <osemwonken18@gmail.com>
2025-12-03 14:55:46 +0100

    added new session page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import NewSession from './pages/NewSession'
 
 function App () {
   return (
-    <div className='w-full h-[200vh]'>
+    <div className='w-full'>
       <div className='fixed bg-radial-[at_90%_90%] from-base-100 to-base-300 to-100 w-screen h-screen z-[-9999]'></div>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<NewSession />} />
-      </Routes>
+      <div className='px-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<NewSession />} />
+        </Routes>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/NewSession.jsx
+++ frontend/src/pages/NewSession.jsx
@@ -1,8 +1,25 @@
+import { CopyIcon, SaveIcon, ArrowBigRight, ShareIcon } from 'lucide-react'
+
 const NewSession = () => {
   return (
-    <div className="w-full h-full flex flex-row items-center justify-center">
-      <form>
-        <textarea name="" id=""></textarea>
+    <div className='w-full h-[70vh] flex flex-col items-center justify-center'>
+      <form className='flex flex-col gap-2 max-w-[500px] w-full'>
+        <label htmlFor='' className='label'>
+          Question:
+        </label>
+        <textarea
+          name=''
+          id=''
+          rows={7}
+          placeholder='Enter Your Question...'
+          className='w-full rounded-lg p-2 text-secondary bg-base-200 border-accent border-4'
+        ></textarea>
+        <div className='w-full flex flex-row justify-end gap-2'>
+          <SaveIcon className="btn stroke-base-300 hover:stroke-secondary size-12 [border-radius:16px]" />
+          <CopyIcon className="btn stroke-base-300 hover:stroke-secondary size-12 [border-radius:16px]" />
+          <ShareIcon className="btn stroke-base-300 hover:stroke-secondary size-12 [border-radius:16px]" />
+          <ArrowBigRight className="btn stroke-base-300 hover:stroke-secondary size-12 [border-radius:16px]" />
+        </div>
       </form>
     </div>
   )
